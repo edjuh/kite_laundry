a2495e00302e309fdea3fb7ae26a586306006b02
Ed de la Rie <pe5ed@hotmail.com>
2025-10-22 08:54:25 +0200

    Delete tube_pattern_configuration_update.py

--- tube_pattern_configuration_update.py
+++
@@ -1,9 +0,0 @@
-
-# Around line 107-108, change 'parameters' to 'config'
-# Before:
-pattern = generate_tube_pattern(parameters)
-instructions = generate_tube_instructions(parameters, pattern)
-
-# After:
-pattern = generate_tube_pattern(config)
-instructions = generate_tube_instructions(config, pattern)
