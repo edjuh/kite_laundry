4d33c2e377d17b0137a3d735c55f07dc9fd00855
Ed de la Rie <pe5ed@hotmail.com>
2025-10-22 10:27:16 +0200

    Delete Core directory

--- Core/pattern_templates/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Pattern template generators for kite laundry designs
-
-from .cone_template import ConePatternTemplate
-from .tube_template import TubePatternTemplate
-
-__all__ = ["TubePatternTemplate", "ConePatternTemplate"]
