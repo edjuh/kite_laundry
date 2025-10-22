f0349510214c71226abc7cf133b36991d49cc95b
Ed de la Rie <pe5ed@hotmail.com>
2025-10-22 18:25:33 +0200

    Auto-update: 2025-10-22 18:25:33

--- update.sh
+++ update.sh
@@ -1,13 +1,17 @@
-#!/bin/bash
+#!/bin/zsh
 cd /Users/ed/kite_laundry
 git checkout feature/new-app
 git pull origin feature/new-app
 if [ -n "$(git status --porcelain)" ]; then
   git add .
-  git commit -m "Auto-update: $(date)"
+  git commit -m "Auto-update: $(date '+%Y-%m-%d %H:%M:%S')"
   git push origin feature/new-app
 fi
-docker stop $(docker ps -q)
-docker rm $(docker ps -a -q)
+if [ -n "$(docker ps -q)" ]; then
+  docker stop $(docker ps -q)
+fi
+if [ -n "$(docker ps -a -q)" ]; then
+  docker rm $(docker ps -a -q)
+fi
 docker build -t kite-laundry .
 docker run -p 5001:5000 -v $(pwd):/app kite-laundry
